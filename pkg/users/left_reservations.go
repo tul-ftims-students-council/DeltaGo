6c7908bace6efd6b26ee26993eae725c93516fba
Piotr Kiełek <62613559+pkielek@users.noreply.github.com>
2022-10-03 16:29:11 +0200

    Update cast to timestamptz instead of date

--- pkg/users/left_reservations.go
+++ pkg/users/left_reservations.go
@@ -12,7 +12,7 @@ func (h handler) GetLeftReservations(c *fiber.Ctx) error {
 	var results []map[string]interface{}
 	format := "2006-01-02 15:04:05"
 	TimeNow := time.Now().UTC()
-	res := h.DB.Table("places").Where("(date_till_expire is null OR date_till_expire < ? ::date) AND is_sold = false", TimeNow.Format(format)).Find(&results)
+	res := h.DB.Table("places").Where("(date_till_expire is null OR date_till_expire < ? ::timestamptz) AND is_sold = false", TimeNow.Format(format)).Find(&results)
 	if res.Error != nil {
 		return utils.HandleResponse(c, fiber.StatusInternalServerError, res.Error.Error())
 	}
