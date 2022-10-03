d1440c1e15988fd11d4743e85e56e9b71506b964
Piotr Kiełek <62613559+pkielek@users.noreply.github.com>
2022-10-03 16:41:24 +0200

    revert datetime format cus it broke + cast as timestamptz

--- pkg/users/left_reservations.go
+++ pkg/users/left_reservations.go
@@ -10,9 +10,9 @@ import (
 func (h handler) GetLeftReservations(c *fiber.Ctx) error {
 
 	var results []map[string]interface{}
-	format := "2006-01-02 15:04:05"
+	format := "2006-01-02T15:04:05.999Z"
 	TimeNow := time.Now().UTC()
-	res := h.DB.Table("places").Where("(date_till_expire is null OR date_till_expire < ? ::date) AND is_sold = false", TimeNow.Format(format)).Find(&results)
+	res := h.DB.Table("places").Where("(date_till_expire is null OR date_till_expire < ? ::timestamptz) AND is_sold = false", TimeNow.Format(format)).Find(&results)
 	if res.Error != nil {
 		return utils.HandleResponse(c, fiber.StatusInternalServerError, res.Error.Error())
 	}
